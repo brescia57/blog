802ba8057b26c66f73149010b346a6046eba061a
Charles Hedji <charleshedji@gmail.com>
2017-02-15 00:16:52 -0500

    migrated to db, created controllers, views and updated models

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,31 @@
 class ArticlesController < ApplicationController
-  def new
+  #Creates an index of all the articles being submitted.
+  def index
+    @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    #render plain: params[ :article].inspect
+    if @article.save
+    redirect_to @article
+    else
+      #rendering is done within the same request
+    render 'new'
+    end
+
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
